254c3867fdd30acde9a13a09da2d1ecd7f1f17b6
skyelighti <skyelighti.dev@gmail.com>
2026-09-17 14:22:28 -0400

    basic main settup

--- Game/Main.cs
+++ Game/Main.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Homework2.Game
+﻿using Homework2.Game;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary.Graphics;
+using System;
+using System.Diagnostics;
+using System.Numerics;
+using System.Reflection.Metadata;
+namespace ShooterTest.Game
 {
-    internal class Main
+    public class Main
     {
+        public static Rectangle ScreenBounds;
+        public static ContentManager contentManager { get; private set; }
+        private CollisionManager collisionManager;
+
+        public static TextureAtlas atlas { get; private set; }
+
+        public Main(ContentManager content)
+        {
+            contentManager = content;
+
+
+            collisionManager = new CollisionManager(content);
+
+        }
+
+        //maybe implement a statemaachine for game state? :P extra work to do if i hate reading!!
+        public void Update(GameTime gameTime, Rectangle screenBounds)
+        {
+            // update everything else here as well?
+            ScreenBounds = screenBounds;
+            collisionManager.Update(gameTime);
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
     }
 }
